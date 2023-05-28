2bfbc9ff3e347e2440fd66bc5f821da27c1f705a
Joshua Sprague <joshua.spragu3@gmail.com>
2023-05-28 12:09:15 -0500

    Remove sample classes.

--- src/web/GlobalUsings.cs
+++ src/web/GlobalUsings.cs
@@ -1,7 +1,6 @@
 // Global using directives
 
 global using cocograph;
-global using cocograph.Data;
 global using cocograph.Domain.Models;
 global using cocograph.Domain.Services;
 global using InfiniteLoop.Extensions;
